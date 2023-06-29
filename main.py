7d49f18dd46a076c8fb3e6ee8de0d7c735c26165
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-06-29 09:52:03 +0900

    update

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from bs4 import BeautifulSoup
 import json
 
 link_dic = {}
-link_dic2 = {}
-
 
 for i in range(1,533):
     link_dic[i] = []
 
 
-for i in range(1,2):
+for i in range(1,533):
 
 
     html = Request("https://www.examtopics.com/discussions/amazon/"+str(i)+"/",headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})  
@@ -28,31 +26,16 @@ for i in range(1,2):
 
     # print (bsObject.head.find("meta", {"name":"description"}))
 
-
-
     # print (bsObject.head.find("meta", {"name":"description"}).get('content'))
 
-    
     for link in bsObject.find_all('a'):
         temp1 = link.text.strip()
         temp2 = link.get('href')
         temp3 = temp1.split()
         if "Exam AWS Certified Solutions Architect - Associate SAA-C03" in temp1:
-            print(temp3[11])
-            if link_dic.get(temp3[11]):
-                if link_dic2.get(temp3[11]):
-                    link_dic2[temp3[11]].append("https://www.examtopics.com"+temp2)
-                else:
-                    link_dic[temp3[11]] = ["https://www.examtopics.com"+temp2]
-            else:
-                link_dic[temp3[11]] = "https://www.examtopics.com"+temp2
+            link_dic[temp3[11]].append("https://www.examtopics.com"+temp2)
 
-print(link_dic)
-
-
+# print(link_dic)
 
 with open('link.json', 'w') as f : 
 	json.dump(link_dic, f, indent=4)
-
-with open('link2.json', 'w') as f : 
-	json.dump(link_dic2, f, indent=4)
