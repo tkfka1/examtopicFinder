5588a956f071c62cabfc9beb7d81881f97c4b02b
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-06-29 09:49:33 +0900

    update

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import json
 link_dic = {}
 link_dic2 = {}
 
+
 for i in range(1,533):
+    link_dic[i] = []
+
+
+for i in range(1,2):
 
 
     html = Request("https://www.examtopics.com/discussions/amazon/"+str(i)+"/",headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})  
@@ -43,8 +48,11 @@ for i in range(1,533):
                 link_dic[temp3[11]] = "https://www.examtopics.com"+temp2
 
 print(link_dic)
+
+
+
 with open('link.json', 'w') as f : 
 	json.dump(link_dic, f, indent=4)
 
-with open('link.json', 'w') as f : 
+with open('link2.json', 'w') as f : 
 	json.dump(link_dic2, f, indent=4)
